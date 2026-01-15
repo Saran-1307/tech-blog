c6566ab968f83d101f2889f50d8f50d46d53a3ed
ransan1507 <saranoff2005@gmail.com>
2026-01-15 15:11:04 +0530

    6-commit

--- ai-blog-agent/frontend/src/supabaseClient.js
+++ ai-blog-agent/frontend/src/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 // USE THE ANON PUBLIC KEY HERE (Not the service key!)
-const supabaseUrl = "YOUR_SUPABASE_PROJECT_URL";
-const supabaseKey = "YOUR_SUPABASE_ANON_PUBLIC_KEY";
+const supabaseUrl = "https://rmidgficnfbtrlzanfyd.supabase.co";
+const supabaseKey = "sb_publishable_iiwQ-ixEeVgHYS4OMDERPA_v_DcWDZp";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
